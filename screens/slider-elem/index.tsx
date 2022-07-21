1258bcc20c9ee97d80aa6969713a7632410f94b6
p.beloborodov <p.beloborodov@els24.com>
2022-07-21 17:49:05 +0700

    add slider scroll

--- screens/slider-elem/index.tsx
+++ screens/slider-elem/index.tsx
@@ -1,15 +1,31 @@
 import React, {FC} from 'react';
-import {StyleSheet, View, Text, Image, Pressable} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Pressable,
+  ScrollView,
+  Dimensions,
+  ScrollViewBase,
+} from 'react-native';
 import Cross from '../../src/assets/img/icon/cross-white.svg';
 import CrossSlide from '../../src/assets/img/icon/cross-slide.svg';
+
 import Swiper from 'react-native-web-swiper';
+import {defaultStyle} from '../../src/common/styles/default';
 import {style} from '../../src/common/styles/variables/style';
 type Props = {};
 
 const SliderElem: FC<Props> = props => {
+  const heightScreen = Dimensions.get('window').height - 90;
+
   return (
-    <View style={styles.container}>
-      <Swiper controlsEnabled={false}>
+    <ScrollView
+      style={[styles.container, {height: heightScreen, position: 'relative'}]}>
+      <Swiper
+        controlsEnabled={false}
+        containerStyle={{height: 288, width: '100%'}}>
         <View style={[styles.slideContainer]}>
           <View style={{height: '100%', width: '100%'}}>
             <Image
@@ -40,7 +56,107 @@ const SliderElem: FC<Props> = props => {
           </Pressable>
         </View>
       </Swiper>
-    </View>
+      <ScrollView
+        style={[styles.horizontal]}
+        horizontal
+        contentOffset={{x: 250, y: 0}}>
+        <Pressable>
+          <View style={styles.horizontalItem}>
+            <Image
+              source={require('../../src/assets/img/img-video/image72.jpg')}
+              resizeMode={'cover'}
+              style={{
+                height: 288,
+                width: 288,
+              }}
+            />
+            <Pressable style={styles.btnCross}>
+              <Cross />
+            </Pressable>
+            <View style={styles.horizontalItemName}>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                Название
+              </Text>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                1/3
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+
+        <Pressable>
+          <View style={styles.horizontalItem}>
+            <Image
+              source={require('../../src/assets/img/img-video/image72.jpg')}
+              resizeMode={'cover'}
+              style={{
+                height: 288,
+                width: 288,
+              }}
+            />
+            <Pressable style={styles.btnCross}>
+              <Cross />
+            </Pressable>
+            <View style={styles.horizontalItemName}>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                Название
+              </Text>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                1/3
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+
+        <Pressable>
+          <View style={styles.horizontalItem}>
+            <Image
+              source={require('../../src/assets/img/img-video/image72.jpg')}
+              resizeMode={'cover'}
+              style={{
+                height: 288,
+                width: 288,
+              }}
+            />
+            <Pressable style={styles.btnCross}>
+              <Cross />
+            </Pressable>
+            <View style={styles.horizontalItemName}>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                Название
+              </Text>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                1/3
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+
+        <Pressable>
+          <View style={styles.horizontalItem}>
+            <Image
+              source={require('../../src/assets/img/img-video/image72.jpg')}
+              resizeMode={'cover'}
+              style={{
+                height: 288,
+                width: 288,
+              }}
+            />
+            <Pressable style={styles.btnCross}>
+              <Cross />
+            </Pressable>
+            <View style={styles.horizontalItemName}>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                Название
+              </Text>
+              <Text style={[defaultStyle.text, {color: style.color.white}]}>
+                1/3
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+      </ScrollView>
+    </ScrollView>
   );
 };
 
@@ -58,22 +174,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 50,
   },
+  horizontalItemName: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderRadius: 4,
+    width: '100%',
+    bottom: 0,
+    backgroundColor: style.color.opacity.black,
+  },
   container: {
-    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    // justifyContent: 'flex-start',
   },
   slideContainer: {
-    height: '40%',
+    // height: '40%',
+    height: 288,
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  slide1: {
-    backgroundColor: 'rgba(20,20,200,0.3)',
-  },
-  slide2: {
-    backgroundColor: 'rgba(20,200,20,0.3)',
-  },
-  slide3: {
-    backgroundColor: 'rgba(200,20,20,0.3)',
   },
   slideAdd: {
     height: '100%',
@@ -85,6 +208,15 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 1,
     borderStyle: 'dashed',
+  },
+  horizontal: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: 20,
+  },
+  horizontalItem: {
+    marginRight: 20,
   },
 });
 
